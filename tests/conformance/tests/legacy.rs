fd877c65276c1cfde1142087a645ff8653ad6faa
Jared Hembrow <jared.hembrow@gmail.com>
2026-08-29 07:36:08 +1000

    E17.02.50 with + while/do-while through object environment

--- tests/conformance/tests/legacy.rs
+++ tests/conformance/tests/legacy.rs
@@ -538,3 +538,13 @@ fn with_if_fixture_present() {
 fn with_if_runs() {
     assert_fixture_runs_declared_targets("es/legacy/with_if");
 }
+
+#[test]
+fn with_while_fixture_present() {
+    assert_fixture_present("es/legacy/with_while");
+}
+
+#[test]
+fn with_while_runs() {
+    assert_fixture_runs_declared_targets("es/legacy/with_while");
+}
