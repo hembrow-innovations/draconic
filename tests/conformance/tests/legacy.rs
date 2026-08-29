09d8e35b3d518f2f5b7bd43a39f72772b41af76d
Jared Hembrow <jared.hembrow@gmail.com>
2026-08-30 08:05:13 +1000

    E17.02.113 with + Proxy defineProperty/getOwnPropertyDescriptor through object environment

--- tests/conformance/tests/legacy.rs
+++ tests/conformance/tests/legacy.rs
@@ -1168,3 +1168,13 @@ fn with_proxy_prototype_fixture_present() {
 fn with_proxy_prototype_runs() {
     assert_fixture_runs_declared_targets("es/legacy/with_proxy_prototype");
 }
+
+#[test]
+fn with_proxy_define_property_fixture_present() {
+    assert_fixture_present("es/legacy/with_proxy_define_property");
+}
+
+#[test]
+fn with_proxy_define_property_runs() {
+    assert_fixture_runs_declared_targets("es/legacy/with_proxy_define_property");
+}
